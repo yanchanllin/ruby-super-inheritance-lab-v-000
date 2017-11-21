9c2e65e844f0d505c09d2405abcf0ffa845460e5
YanC Lin <yonalin1@gmail.com>
2017-11-21 19:02:41 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,10 +1,10 @@
 class ChattyStudent < Student
 
   def hello
-    super puts"How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    super.pop puts"How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   def raise_hand
-   super.ten.times
+   super.10.times
   end
 
 end
